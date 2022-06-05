05550c70ac04fe515e84e3bd45c7c9fae0833739
John Smith <rintran720@gmail.com>
2022-06-05 22:57:04 +0700

    implement simple express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+dotenv.config();
+
+const app = express();
+app.use(helmet());
+app.use(morgan("combine"));
+
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Application start at port ${PORT}`);
+});
