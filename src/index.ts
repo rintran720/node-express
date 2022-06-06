165a28678f684ff27eb35496b8c6548a991bf92b
John Smith <rintran720@gmail.com>
2022-06-06 08:49:39 +0700

    use compression

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
+import compression from "compression";
 import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+
 dotenv.config();
 
 const app = express();
 app.use(helmet());
 app.use(morgan("combine"));
+app.use(
+  compression({
+    level: 6,
+    threshold: 100 * 1000, // bytes
+  })
+);
 
 const PORT = Number(process.env.PORT) || 3000;
 
